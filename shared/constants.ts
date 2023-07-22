e5b8173a6cba7ee3ab7756b717bc12816f6d8ef8
Matthew Coombs <matthew@Matthews-MacBook-Pro.local>
2023-07-22 16:31:27 -0400

    adding postgres db connection on dao files

---
+++ shared/constants.ts
@@ -0,0 +1,11 @@
+export function generateWelcomeCopy(username: string) {
+   return `Hello ${username}, welcome to the server!\n
+I'll be your helpful assistant while you're here.\n
+To access my features please utilize the **/** to see a list of my various commands.\n
+For instance if you need an answer to a quick question you can use my **/magic_conch** command.\n
+If you want an image generated you can use **/magic_conch_image** command.\n
+If you want to just chat you can use my **/lets_chat** command.\n
+Have fun and enjoy your time here :blush:!`;
+}
+
+export const GENERAL_CHANNEL = 'general';
