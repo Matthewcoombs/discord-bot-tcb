41ff824e717e5bf4c9c8e4192f60a6887c375c96
Matthew Coombs <matthew@Matthews-MacBook-Pro.local>
2023-07-27 10:02:52 -0400

    improving image generation logic and add option for multiple images

--- commands/fun/magic_conch_image.ts
+++ commands/fun/magic_conch_image.ts
@@ -1,34 +1,48 @@
 import { ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder, SlashCommandStringOption } from "discord.js";
 import { Command } from "../../shared/discord-js-types";
 import { OpenAi } from "../..";
+import { ImagesResponse } from "openai";
+
+function generateImageEmbeds(generatedImages: ImagesResponse, username: string, description: string) {
+    const embeds = generatedImages.data.map(image => {
+        const imageUrl = image?.url as string;
+        return new EmbedBuilder()
+            .setURL(imageUrl)
+            .setImage(imageUrl);
+    });
+
+    embeds[0].setTitle(`${username}'s image(s) of ${description}`);
+
+    return embeds;
+}
 
 const aiImageGenerateCommand: Command = {
 	data: new SlashCommandBuilder()
 		.setName('magic_conch_image')
 		.setDescription('Ask the all knowing magic conch shell to generate an image')
-		.addStringOption((option: SlashCommandStringOption) =>
-			option.setName('description')
+		.addStringOption((strOption: SlashCommandStringOption) =>
+			strOption.setName('description')
 				.setDescription('Describe the image you want generated')
-				.setRequired(true)),
+				.setRequired(true))
+        .addIntegerOption((intOption) =>
+            intOption.setName('image_count')
+                .setDescription('The amount of images to generate')
+                .addChoices(
+                    {name: '1', value: 1},
+                    { name: '2', value: 2 },
+                    { name: '3', value: 3},
+                    { name: '4', value: 4})),
 	async execute(interaction: ChatInputCommandInteraction) {
         const username = interaction.user.username;
 		const description = await interaction.options.getString('description', true).toLowerCase();
+        const imageCount = await interaction.options.getInteger('image_count', false);
         await interaction.reply(`${username} asked for an image, so I'm working on it :art:...`);
         await OpenAi.createImage({
             prompt: description,
-            n: 4,
+            n: imageCount,
             })
             .then(async completion => {
-                // const imageUrl = completion.data.data[0].url as string;
-                const imageUrls = completion.data.data.map( genImage => { return genImage.url as string });
-                const embeds = imageUrls.map(imageUrl => {
-                    return  new EmbedBuilder()
-                    .setTitle(`${username}'s Image of ${description}`)
-                    // .setURL(imageUrl)
-                    .setImage(imageUrl)
-                });
-
-                embeds[0].setTitle(`${username}'s Image of ${description}`);
+                const embeds = generateImageEmbeds(completion.data, username, description);
                 await interaction.editReply({ 
                     content: `Here is your picture ${username} :blush:!`,
                     embeds: embeds});
