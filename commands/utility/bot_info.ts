251475a89bb075eeeecc00a4cc5f6868fdd7684a
Matthew Coombs <matthew_k_coombs@outlook.com>
2024-03-06 11:57:17 -0500

    adding bot info command

---
+++ commands/utility/bot_info.ts
@@ -0,0 +1,31 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { Command } from "../../shared/discord-js-types";
+
+const userInfoCommand: Command = {
+	data: new SlashCommandBuilder()
+		.setName('bot_info')
+		.setDescription('Provides information about the server bot'),
+	async execute(interaction: ChatInputCommandInteraction) {
+        let botInfoMsg = `
+Here are the following commands available for use:\n
+`;
+        // filtering out commands with administrator permissions set.
+        const filteredCommands = interaction.client.commands.filter(command => {
+            if (command.data.default_member_permissions !== '8') {
+                return command;
+            }
+        });
+
+        for (const command of filteredCommands) {
+            const { name, description} = command[1].data;
+            botInfoMsg += `commandName: [${name}] - description: [${description}]\n`;
+        }
+
+		await interaction.reply({
+			content: botInfoMsg,
+			ephemeral: true,
+		});
+	},
+};
+
+export = userInfoCommand;
