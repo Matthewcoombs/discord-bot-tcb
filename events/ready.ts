9a8170e451be610dc4d3cc9a833169f68e77a029
Matthew Coombs <matthew_k_coombs@outlook.com>
2024-03-11 10:25:36 -0400

    8.0.0 adding development actions and updating ready event

--- events/ready.ts
+++ events/ready.ts
@@ -1,10 +1,25 @@
 import { Client, Events } from "discord.js";
 import { Command } from "../shared/discord-js-types";
+import * as fs from 'fs';
 
 const clientReadyEvent: Command = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client: Client) {
+		const botDisplayName = client.user?.displayName as string;
+
+		const packageJsonPath = './package.json';
+		const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+
+		const regex = /\b\d+\.\d+\.\d+\b/;
+        const match = botDisplayName.match(regex);
+        const botDisplayVersion = match ? match[0] : null;
+
+		if (botDisplayVersion && botDisplayVersion !== packageJson.version) {
+			const updatedBotName = botDisplayName.replace(botDisplayVersion, packageJson.version);
+			console.log(`Bot version update detected. Updating bot [name]: ${updatedBotName}`);
+			// await client.user?.setUsername(updatedBotName);
+		}
 		console.log(`Ready! Logged in as ${client?.user?.tag}`);
 	}
 };
